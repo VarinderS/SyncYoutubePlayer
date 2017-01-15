41a13f1a5970bddea515c143b290c472302f833d
Varinder Singh <varinder@kudosweb.com>
2017-01-15 22:29:53 +1300

    Added in observable socket

--- src/client/application.js
+++ src/client/application.js
@@ -1,1 +1,18 @@
 import "./application.scss";
+import * as services from "./services";
+
+// playgroud
+services.server.on$("test")
+	.map(d => d + " transformed")
+	.subscribe(item => {
+		console.log(`Got ${item} from server`);
+	});
+
+services.server.status$.subscribe(state => console.log(state));
+
+// auth
+
+// components
+
+// bootstrap
+services.socket.connect();
---
+++ src/client/services.js
@@ -0,0 +1,13 @@
+import io from "socket.io-client";
+import { ObservableSocket } from "shared/observable-socket";
+
+export const socket = io({ autoConnect: false });
+
+// create socket wrapper
+export const server = new ObservableSocket(socket);
+
+// create playlist store
+
+// create user store
+
+// create chat store
